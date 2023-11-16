1a8285cd24a0fd3a68b85d6f72b20031e906f059
Vedblade <18503283+yad439@users.noreply.github.com>
2023-11-16 14:03:59 +0000

    Add convertion to dollars

--- src/main.zig
+++ src/main.zig
@@ -9,46 +9,43 @@ pub fn main() !void {
         const check = gpa.deinit();
         if (check != .ok) std.debug.print("Memory leak detected\n", .{});
     }
-    const stdout_file = std.io.getStdOut().writer();
 
     var args = try std.process.argsWithAllocator(allocator);
     defer args.deinit();
     _ = args.skip(); // skip program name
     const mode = args.next();
     if (mode == null) {
-        try stdout_file.print("Need to specify mode\n", .{});
+        std.debug.print("Need to specify mode\n", .{});
         return;
     }
     const filename_optional = args.next();
     if (filename_optional == null) {
-        try stdout_file.print("Need to specify input file name\n", .{});
+        std.debug.print("Need to specify input file name\n", .{});
         return;
     }
     const filename = filename_optional.?;
     const output_optional = args.next();
-    var allocated_second = output_optional == null;
+    const allocated_second = output_optional == null;
     const output_name = if (output_optional == null) blk1: {
-        allocated_second = true;
         const buf = try allocator.alloc(u8, filename.len + 4);
         @memcpy(buf[0..filename.len], filename);
         @memcpy(buf[filename.len..], ".out");
         break :blk1 buf;
     } else blk2: {
-        allocated_second = false;
         break :blk2 output_optional.?;
     };
     defer if (allocated_second) {
         allocator.free(output_name);
     };
     const input_file = std.fs.cwd().openFile(filename, .{}) catch |e| {
-        try stdout_file.print("Could not open file: {}\n", .{e});
+        std.debug.print("Could not open file: {}\n", .{e});
         return;
     };
     defer input_file.close();
     var input_reader = std.io.bufferedReader(input_file.reader());
     const input = input_reader.reader();
     const output_file = std.fs.cwd().createFile(output_name, .{}) catch |e| {
-        try stdout_file.print("Could not open file: {}\n", .{e});
+        std.debug.print("Could not open file: {}\n", .{e});
         return;
     };
     defer output_file.close();
@@ -57,6 +54,12 @@ pub fn main() !void {
     defer output_writer.flush() catch |err| {
         std.debug.print("Can't write file: {}\n", .{err});
     };
+    if (std.mem.eql(u8, mode.?, "l")) {
+        try convertToBraces(input, output);
+    } else try convertToDollars(input, output);
+}
+
+fn convertToBraces(input: std.io.BufferedReader(4096, std.fs.File.Reader).Reader, output: std.io.BufferedWriter(4096, std.fs.File.Writer).Writer) !void {
     var state = State.DEFAULT;
     while (true) {
         const char = input.readByte() catch |err| {
@@ -102,12 +105,37 @@ pub fn main() !void {
                     try output.writeAll("\\]");
                     state = .DEFAULT;
                 } else {
-                    try stdout_file.print("Mailformed input\n", .{});
-                    try output.writeAll("\\$");
+                    std.debug.print("Mailformed input\n", .{});
+                    try output.writeByte('$');
                     try output.writeByte(char);
                     state = .BLOCK;
                 }
             },
+        }
+    }
+}
+
+fn convertToDollars(input: std.io.BufferedReader(4096, std.fs.File.Reader).Reader, output: std.io.BufferedWriter(4096, std.fs.File.Writer).Writer) !void {
+    while (true) {
+        const char = input.readByte() catch |err| {
+            if (err == error.EndOfStream) {
+                break;
+            } else {
+                return err;
+            }
+        };
+        if (char == '\\') {
+            const second_char = try input.readByte();
+            switch (second_char) {
+                '(', ')' => try output.writeByte('$'),
+                '[', ']' => try output.writeAll("$$"),
+                else => {
+                    try output.writeByte('\\');
+                    try output.writeByte(second_char);
+                },
+            }
+        } else {
+            try output.writeByte(char);
         }
     }
 }
